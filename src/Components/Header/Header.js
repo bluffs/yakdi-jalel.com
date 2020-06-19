3720aabdc341a0b8fbb423803936c21103c27dad
Jalel <yakdijalel@hotmail.com>
2020-06-19 14:53:56 +0200

    :tada: 1st commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Header.css';
+
+const header = () => {
+    return (
+        <div className={classes.Header}>
+            This is the header
+        </div>
+    );
+}
+
+export default header;
