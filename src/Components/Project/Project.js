4104633c81fe229ca07147795eb166c32dfd0935
Jalel <yakdijalel@hotmail.com>
2020-06-30 17:13:23 +0200

    :lipstick: add moving circle on the project image

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './Project.css';
+import Circle from '../../images/circle.png';
 
 const project = (props) => {
     return (
         <div className='Project'>
             <div className='LeftProject'>
                 <img className="ImageProject" src={require('../../images/' + props.image)} alt={props.title} />
+                <img className="Circle" src={Circle} alt="circle" />
             </div>
             <div className='RightProject'>
                 <div className='Title' >{props.title}</div>
