82350e6af303682e6b369feec589b26c9c4b63b6
Jalel <yakdijalel@hotmail.com>
2020-07-03 15:37:34 +0200

    :lipstick: add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header/Header';
 import './App.css';
 import Wrapper from './Containers/Wrapper/Wrapper';
 import Projects from './Containers/Projects/Projects';
+import Footer from './Components/Footer/Footer';
 
 const app = () => {
   return (
@@ -13,6 +14,7 @@ const app = () => {
       <div className="Polygontop" ></div>
       <Projects />
       <div className="Polygonbot" ></div>
+      <Footer />
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            © Jalel Yakdi 2020   //  Développeur Fullstack   //  
+        </div>
+    );
+}
+
+export default Footer;
