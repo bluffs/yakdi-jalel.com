e35aa0ed530c1e2cf7117a58b42fff9d50a6517c
Jalel <yakdijalel@hotmail.com>
2020-07-07 19:13:20 +0200

    :rocket: pushing build dir

---
+++ build/precache-manifest.81874c9df70b64ff3213f0c979f1efc9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efd4e5c172c5827f3c9d8785c49ffc2",
+    "url": "/yakdi-jalel.com/index.html"
+  },
+  {
+    "revision": "e1c0d0faed1eed8307eb",
+    "url": "/yakdi-jalel.com/static/css/main.e1f4e0f2.chunk.css"
+  },
+  {
+    "revision": "1cebffe913021743d861",
+    "url": "/yakdi-jalel.com/static/js/2.1a5cd923.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yakdi-jalel.com/static/js/2.1a5cd923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c0d0faed1eed8307eb",
+    "url": "/yakdi-jalel.com/static/js/main.3da0e540.chunk.js"
+  },
+  {
+    "revision": "1a66c75337f0d44c7132",
+    "url": "/yakdi-jalel.com/static/js/runtime-main.be6f15ab.js"
+  },
+  {
+    "revision": "299ab1aa01a5e68fb919431e5fbe9ef0",
+    "url": "/yakdi-jalel.com/static/media/Alphabot.299ab1aa.png"
+  },
+  {
+    "revision": "39c06054b13fdeeebbe0f9a018bb31dd",
+    "url": "/yakdi-jalel.com/static/media/RedTetris.39c06054.png"
+  },
+  {
+    "revision": "500d3fc523cfb11f0ed4427dc3e80d73",
+    "url": "/yakdi-jalel.com/static/media/arrow.500d3fc5.png"
+  },
+  {
+    "revision": "a6a307f76d30831db534019a64d49165",
+    "url": "/yakdi-jalel.com/static/media/bonjour.a6a307f7.svg"
+  },
+  {
+    "revision": "db13b7a0ba3d2610637131bf06e661b7",
+    "url": "/yakdi-jalel.com/static/media/circle.db13b7a0.png"
+  },
+  {
+    "revision": "476be65264ed3be4b511e148ea57ef0a",
+    "url": "/yakdi-jalel.com/static/media/dev-design.476be652.svg"
+  },
+  {
+    "revision": "578d153d26d31034ad0138b1fee8aeeb",
+    "url": "/yakdi-jalel.com/static/media/intro.578d153d.jpg"
+  }
+]);
